cedc9c29d5f0e4d94d174b97ad52f0108cb9d33a
Sarah Saroufim <saros031@newschool.edu>
2024-02-26 19:24:39 -0500

    added content on click

--- arena.js
+++ arena.js
@@ -1,193 +1,241 @@
 // This allows us to process/render the descriptions, which are in Markdown!
 // More about Markdown: https://en.wikipedia.org/wiki/Markdown
-let markdownIt = document.createElement('script')
-markdownIt.src = 'https://cdn.jsdelivr.net/npm/markdown-it@14.0.0/dist/markdown-it.min.js'
-document.head.appendChild(markdownIt)
+let markdownIt = document.createElement("script");
+markdownIt.src =
+  "https://cdn.jsdelivr.net/npm/markdown-it@14.0.0/dist/markdown-it.min.js";
+document.head.appendChild(markdownIt);
 
 // Okay, Are.na stuff!
-let channelSlug = 'nirvaan' // The “slug” is just the end of the URL
+let channelSlug = "nirvaan"; // The “slug” is just the end of the URL
+
+// this holds all block data from are.na api
+let blockdata = {};
+
+// popup block titles
+let popup = document.querySelector(".popup");
+popup.style.display = "none"; // Sets initial display to none;
+let popcontainer = document.querySelector(".popcontainer");
+popcontainer.style.display = "none";
+
+let windowPop = document.querySelector(".pop");
+windowPop.style.display = "none";
+
+function openWindowPop(circle) {
+  const { title, description, link, image } = {
+    title: circle.getAttribute("data-key"),
+    description: circle.getAttribute("data-description"),
+    link: `https://www.are.na/block/${circle.getAttribute("data-id")}`,
+    image: circle.getAttribute("data-image"),
+  };
+
+  document.getElementById("title").innerHTML = title === null ? "" : title;
+  document.getElementById("description").innerHTML =
+    description === null ? "" : description;
+  document.getElementById("link").href = link;
+
+  if (image == "null") {
+    document.getElementById("image").style.display = "none";
+  } else {
+    document.getElementById("image").style.display = "block";
+    document.getElementById("image").src = image;
+  }
+
+  windowPop.style.display = "flex";
+  popcontainer.style.display = "grid";
+}
+
+function closeWindowPop() {
+  windowPop.style.display = "none";
+  popcontainer.style.display = "none";
+}
 
 // First, let’s lay out some *functions*, starting with our basic metadata:
 let placeChannelInfo = (data) => {
-	// Target some elements in your HTML:
-	let channelTitle = document.getElementById('channel-title')
-	let channelDescription = document.getElementById('channel-description')
-	let channelCount = document.getElementById('channel-count')
-	let channelLink = document.getElementById('channel-link')
+  // Target some elements in your HTML:
+  let channelTitle = document.getElementById("channel-title");
+  let channelDescription = document.getElementById("channel-description");
+  let channelCount = document.getElementById("channel-count");
+  let channelLink = document.getElementById("channel-link");
 
-	// Then set their content/attributes to our data:
-	channelTitle.innerHTML = data.title
-	channelDescription.innerHTML = window.markdownit().render(data.metadata.description) // Converts Markdown → HTML
-	channelCount.innerHTML = data.length
-	channelLink.href = `https://www.are.na/channel/${channelSlug}`
-}
+  // Then set their content/attributes to our data:
+  channelTitle.innerHTML = data.title;
+  channelDescription.innerHTML = window
+    .markdownit()
+    .render(data.metadata.description); // Converts Markdown → HTML
+  channelCount.innerHTML = data.length;
+  channelLink.href = `https://www.are.na/channel/${channelSlug}`;
+};
 
 // Then our big function for specific-block-type rendering:
 let renderBlock = (block) => {
-	// To start, a shared `ul` where we’ll insert all our blocks
-	let channelBlocks = document.getElementById('channel-blocks')
+  // To start, a shared `ul` where we’ll insert all our blocks
+  let channelBlocks = document.getElementById("channel-blocks");
 
-	// channelBlocks.classList.add('circles');
+  // Links!
 
-	// Links!
-
-	if (block.class == 'Link') {
-		let linkItem =
-			`
-			<li class="circle">
-				<!-- <p><em>Link</em></p> -->
+  if (block.class == "Link") {
+    let linkItem = `
+			<li class="circle" data-key="${block.title}" data-id="${block.id}" data-image="${block.image ? block.image.display.url : null}" data-description="${block.description_html}">
 				<picture>
-					<source media="(max-width: 428px)" srcset="${ block.image.thumb.url }">
-					<source media="(max-width: 640px)" srcset="${ block.image.large.url }">
-					<img src="${ block.image.original.url }">
+					<source media="(max-width: 428px)" srcset="${block.image.thumb.url}">
+					<source media="(max-width: 640px)" srcset="${block.image.large.url}">
+					<img src="${block.image.original.url}">
 				</picture>
-				<!-- <h3>${ block.title }</h3>
-				${ block.description_html }
-				<p><a href="${ block.source.url }">See the original ↗</a></p> -->
 			</li>
-			`
-		channelBlocks.insertAdjacentHTML('beforeend', linkItem)
-	}
+			`;
+    channelBlocks.insertAdjacentHTML("beforeend", linkItem);
+  }
 
-	// Images!
-	else if (block.class == 'Image') {
-        let imageItem =
-        `
-            <li class="circle">
+  // Images!
+  else if (block.class == "Image") {
+    let imageItem = `
+            <li class="circle" data-key="${block.title}" data-id="${block.id}" data-image="${block.image ? block.image.display.url : null}" data-description="${block.description_html}">
                 <img src="${block.image.large.url}" alt="${block.title}" by "${block.user.fullname}">
-                <!-- <figcaption>${block.title}</fig> -->
             </li>
-        `
-        channelBlocks.insertAdjacentHTML('beforeend', imageItem)
-	}
+        `;
+    channelBlocks.insertAdjacentHTML("beforeend", imageItem);
+  }
 
-	// Text!
-	else if (block.class == 'Text') {
-		let textItem =
-			`
-			<li class="circle">
+  // Text!
+  else if (block.class == "Text") {
+    let textItem = `
+			<li class="circle" data-key="${block.title}" data-id="${block.id}" data-image="${block.image ? block.image.display.url : null}" data-description="${block.description_html}">
 				<blockquote>
 				${block.content_html}
 				</blockquote>
 			</li>
-			`
-		channelBlocks.insertAdjacentHTML('beforeend', textItem)
-	}
+			`;
+    channelBlocks.insertAdjacentHTML("beforeend", textItem);
+  }
 
-	// Uploaded (not linked) media…
-	else if (block.class == 'Attachment') {
-	let attachment = block.attachment.content_type // Save us some repetition
+  // Uploaded (not linked) media…
+  else if (block.class == "Attachment") {
+    let attachment = block.attachment.content_type; // Save us some repetition
 
-		// Uploaded videos!
-		if (attachment.includes('video')) {
-			// …still up to you, but we’ll give you the `video` element:
-			let videoItem =
-				`
-				<li class="circle">
-					<!-- <p><em>Video</em></p> -->
-					<video controls src="${ block.attachment.url }"></video>
+    // Uploaded videos!
+    if (attachment.includes("video")) {
+      // …still up to you, but we’ll give you the `video` element:
+      let videoItem = `
+				<li class="circle" data-key="${block.title}" data-id="${block.id}" data-image="${block.image ? block.image.display.url : null}" data-description="${block.description_html}">
+					<video controls src="${block.attachment.url}"></video>
 				</li>
-				`
-			channelBlocks.insertAdjacentHTML('beforeend', videoItem)
-			// More on video, like the `autoplay` attribute:
-			// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/video
-		}
+				`;
+      channelBlocks.insertAdjacentHTML("beforeend", videoItem);
+      // More on video, like the `autoplay` attribute:
+      // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/video
+    }
 
-        // Uploaded PDFs!
-        else if (attachment.includes('pdf')) {
-            let pdfItem =
-            `
-				<li class="circle">
+    // Uploaded PDFs!
+    else if (attachment.includes("pdf")) {
+      let pdfItem = `
+				<li class="circle" data-key="${block.title}" data-id="${block.id}" data-image="${block.image ? block.image.display.url : null}" data-description="${block.description_html}">
                     <a href="${block.attachment.url}">
                         <figure>
                             <img src="${block.image.large.url}" alt="${block.title}">
-                            <!-- <figcaption>${block.title}</figcaption> -->
                         </figure>
                     </a>
                 </li>
-            `
-        channelBlocks.insertAdjacentHTML('beforeend', pdfItem);
-        }
+            	`;
+      channelBlocks.insertAdjacentHTML("beforeend", pdfItem);
+    }
 
-		// Uploaded audio!
-		else if (attachment.includes('audio')) {
-			// …still up to you, but here’s an `audio` element:
-			let audioItem =
-				`
-				<li class="circle">
-					<!-- <p><em>Audio</em></p> -->
-					<audio controls src="${ block.attachment.url }"></video>
+    // Uploaded audio!
+    else if (attachment.includes("audio")) {
+      // …still up to you, but here’s an `audio` element:
+      let audioItem = `
+				<li class="circle" data-key="${block.title}" data-id="${block.id}" data-image="${block.image ? block.image.display.url : null}" data-description="${block.description_html}">
+					<audio controls src="${block.attachment.url}"></video>
 				</li>
-				`
-			channelBlocks.insertAdjacentHTML('beforeend', audioItem)
-			// More on audio: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/audio
-		}
-	}
+				`;
+      channelBlocks.insertAdjacentHTML("beforeend", audioItem);
+      // More on audio: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/audio
+    }
+  }
 
-	// Linked media…
-	else if (block.class == 'Media') {
-		let embed = block.embed.type
+  // Linked media…
+  else if (block.class == "Media") {
+    let embed = block.embed.type;
 
-		// Linked video!
-		if (embed.includes('video')) {
-			// …still up to you, but here’s an example `iframe` element:
-			let linkedVideoItem =
-				`
-				<li class="circle">
-					<!-- <p><em>Linked Video</em></p> -->
-					${ block.embed.html }
+    // Linked video!
+    if (embed.includes("video")) {
+      // …still up to you, but here’s an example `iframe` element:
+      let linkedVideoItem = `
+				<li class="circle" data-key="${block.title}" data-id="${block.id}" data-image="${block.image ? block.image.display.url : null}" data-description="${block.description_html}">
+					${block.embed.html}
 				</li>
-				`
-			channelBlocks.insertAdjacentHTML('beforeend', linkedVideoItem)
-			// More on iframe: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe
-		}
+				`;
+      channelBlocks.insertAdjacentHTML("beforeend", linkedVideoItem);
+      // More on iframe: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe
+    }
 
-		// Linked audio!
-		else if (embed.includes('rich')) {
-			// …up to you!
-		}
-	}
-}
+    // Linked audio!
+    else if (embed.includes("rich")) {
+      // …up to you!
+    }
+  }
+};
 
 // It‘s always good to credit your work:
-let renderUser = (user, container) => { // You can have multiple arguments for a function!
-	let userAddress =
-		`
+let renderUser = (user, container) => {
+  // You can have multiple arguments for a function!
+  let userAddress = `
 		<address>
-			<h3>${ user.first_name }</h3>
-			<p><a href="https://are.na/${ user.slug }">are.na profile</a></p>
+			<h3>${user.first_name}</h3>
+			<p><a href="https://are.na/${user.slug}">are.na profile</a></p>
 		</address>
-		`
-	container.insertAdjacentHTML('beforeend', userAddress)
-}
+		`;
+  container.insertAdjacentHTML("beforeend", userAddress);
+};
+fetch(`https://api.are.na/v2/channels/${channelSlug}?per=100`, {
+  cache: "no-store",
+})
+  .then((response) => response.json()) // Return it as JSON data
+  .then((data) => {
+    // Do stuff with the data
+    console.log(data); // Always good to check your response!
+    placeChannelInfo(data); // Pass the data to the first function
 
-// Now that we have said what we can do, go get the data:
-fetch(`https://api.are.na/v2/channels/${channelSlug}?per=100`, { cache: 'no-store' })
-	.then((response) => response.json()) // Return it as JSON data
-	.then((data) => { // Do stuff with the data
-		console.log(data) // Always good to check your response!
-		placeChannelInfo(data) // Pass the data to the first function
-
-		// Loop through the `contents` array (list), backwards. Are.na returns them in reverse!
-		data.contents.reverse().forEach((block) => {
-			console.log(block) // The data for a single block
-			renderBlock(block) // Pass the single block data to the render function
-		})
+    // Loop through the `contents` array (list), backwards. Are.na returns them in reverse!
+    data.contents
+      .reverse()
+      .slice(0, 40)
+      .forEach((block) => {
+        console.log(block); // The data for a single block
+        blockdata[`${block.title}.${block.id}`] = block;
+        renderBlock(block); // Pass the single block data to the render function
+      });
 
 // random animation
 document.querySelectorAll(".circle").forEach((circle) => {
 
-	// variable that keeps track of mouse hovering over circle
+	// A variable that keeps track whether mouse of hovering over the circle.
 	let hovering = false;
 
 	circle.addEventListener("mouseover", () => {
-		hovering = true;
-		circle.classList.add("hovering"); // adds hovering class
+	hovering = true;
+	circle.classList.add("hovering"); // adds hovering class
+
+	popup.innerText = circle.getAttribute("data-key");
+
+	if (popup.innerText.length > 20) {
+		popup.innerText = popup.innerText.slice(0, 20) + " ...";
+	}
+
+	popup.style.display = "block";
+
+	popup.style.top = `${circle.offsetTop - 64 / 4}px`;
+	popup.style.left = `${circle.offsetLeft - 64 / 4}px`;
+	});
+
+	circle.addEventListener("click", () => {
+	openWindowPop(circle);
 	});
 
 	circle.addEventListener("mouseout", () => {
-		hovering = false;
-		circle.classList.remove("hovering"); // removes hovering class
+	hovering = false;
+	// showTitleAt();
+	circle.classList.remove("hovering"); // removes hovering class
+	popup.style.display = "none";
 	});
 
 	let randomDuration = 5;
@@ -203,49 +251,49 @@ document.querySelectorAll(".circle").forEach((circle) => {
 	circle.style.setProperty("--random-duration", `${randomDuration}s`);
 	circle.style.setProperty("--random-delay", `0s`);
 	circle.style.setProperty(
-		"--random-x",
-		`${(randomX / window.innerWidth) * 100}vw`
+	"--random-x",
+	`${(randomX / window.innerWidth) * 100}vw`
 	);
 	circle.style.setProperty(
-		"--random-y",
-		`${(randomY / window.innerHeight) * 100}vh`
+	"--random-y",
+	`${(randomY / window.innerHeight) * 100}vh`
 	);
 	circle.style.setProperty("--random-move-x", `${randomMoveX}px`);
 	circle.style.setProperty("--random-move-y", `${randomMoveY}px`);
 
 	function updatePosition() {
-		if (!animationPaused && !hovering) {
+	if (!animationPaused && !hovering) {
 		randomX += randomMoveX;
 		randomY += randomMoveY;
 		if (randomX < 0 || randomX > maxX) {
-			randomMoveX *= -1; // reverse x direction
+		randomMoveX *= -1; // reverse x direction
 		}
 
 		if (randomY < 0 || randomY > maxHeight) {
-			randomMoveY *= -1; // reverse y direction
+		randomMoveY *= -1; // reverse y direction
 		}
 
 		circle.style.setProperty(
-			"--random-x",
-			`${(randomX / window.innerWidth) * 100}vw`
+		"--random-x",
+		`${(randomX / window.innerWidth) * 100}vw`
 		);
 		circle.style.setProperty(
-			"--random-y",
-			`${(randomY / window.innerHeight) * 100}vh`
+		"--random-y",
+		`${(randomY / window.innerHeight) * 100}vh`
 		);
-		}
+	}
 	}
 
 	setInterval(updatePosition, 200); // update position every 5 secs
-	});
-	
+});
 
 // Also display the owner and collaborators:
 let channelUsers = document.getElementById("channel-users"); // Show them together
 data.collaborators.forEach((collaborator) =>
-renderUser(collaborator, channelUsers));
-renderUser(data.user, channelUsers);
-});
+	renderUser(collaborator, channelUsers)
+	);
+	renderUser(data.user, channelUsers);
+	});
 
 // variable that tracks animation state
 let animationPaused = false;
